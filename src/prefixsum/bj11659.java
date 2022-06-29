5ce87bf258b68a9b050fc677e1cdc9490ef2bd58
dusgn <33680794+gilthub@users.noreply.github.com>
2022-06-29 22:08:47 +0900

    20220629 commit

--- src/prefixsum/bj11659.java
+++ src/prefixsum/bj11659.java
@@ -1,7 +1,27 @@
 package prefixsum;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
 public class bj11659 {
-    public static void main(String[] args) {
-        System.out.println("zzz");
+    public static void main(String[] args) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        int n = Integer.parseInt(stringTokenizer.nextToken());
+        int m = Integer.parseInt(stringTokenizer.nextToken());
+        long[] sum = new long[n + 1];
+        stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+        for (int i = 1; i <= n; i++) {
+            sum[i] = sum[i - 1] + Integer.parseInt(stringTokenizer.nextToken());
+        }
+        for (int q = 0; q < m; q++) {
+            stringTokenizer = new StringTokenizer(bufferedReader.readLine());
+            int i = Integer.parseInt(stringTokenizer.nextToken());
+            int j = Integer.parseInt(stringTokenizer.nextToken());
+            System.out.println(sum[j] - sum[i-1]);
+        }
+
     }
 }
