0f456fd6b5c2e32b410f9a100162332616693250
dusgn <33680794+gilthub@users.noreply.github.com>
2022-08-17 22:31:49 +0900

    Merge 정렬 기본 수정

--- src/sort/Merge.java
+++ src/sort/Merge.java
@@ -4,40 +4,41 @@ public class Merge {
     public static void main(String[] args) {
         int[] A = {1, 2, 3, 5};
         int[] B = {4, 6, 7, 8};
-        int[] result = new int[8];
-        merge(result, A, B);
-        print(result);
+        int[] merge = merge(A, B);
+        print(merge);
     }
 
-    private static void merge(int[] res, int[] a, int[] b) {
+    private static int[] merge(int[] a, int[] b) {
+        int[] result = new int[8];
         int idx = 0;
         int a_idx = 0;
         int b_idx = 0;
         while (a_idx < a.length && b_idx < b.length) {
             if (a[a_idx] < b[b_idx]) {
-                res[idx] = a[a_idx];
+                result[idx] = a[a_idx];
                 a_idx++;
                 idx++;
             } else {
-                res[idx] = b[b_idx];
+                result[idx] = b[b_idx];
                 b_idx++;
                 idx++;
             }
             if (a_idx == a.length) {
                 while (b_idx < b.length) {
-                    res[idx] = b[b_idx];
+                    result[idx] = b[b_idx];
                     b_idx++;
                     idx++;
                 }
             }
             if (b_idx == b.length) {
                 while (a_idx < a.length) {
-                    res[idx] = a[a_idx];
+                    result[idx] = a[a_idx];
                     a_idx++;
                     idx++;
                 }
             }
         }
+        return result;
     }
 
     private static void print(int[] array) {
